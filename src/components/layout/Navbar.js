265693c174846efe9df3a11fe9b9dbd5b4f67efa
unknown <rohit.raj.bhandari07@gmail.com>
2020-08-27 01:04:03 +0300

    created the projectlist and content of dashboard

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
+
 const Navbar = () => {
   return (
     <nav className='nav-wrapper blue draken-3'>
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div>
+      <div className='project-list section'>
+        <div className='card z-depth-0 project-summary'>
+          <div className='card-content gery-text-darken-3'>
+            <span className='cart-title'>Project title</span>
+            <p>Posted by the Rohit Bhandari</p>
+            <p className='grey-text'>26rd august , 2am</p>
+          </div>
+        </div>
+
+        <div className='card z-depth-0 project-summary'>
+          <div className='card-content gery-text-darken-3'>
+            <span className='cart-title'>Project title</span>
+            <p>Posted by the Rohit Bhandari</p>
+            <p className='grey-text'>26rd august , 2am</p>
+          </div>
+        </div>
+
+        <div className='card z-depth-0 project-summary'>
+          <div className='card-content gery-text-darken-3'>
+            <span className='cart-title'>Project title</span>
+            <p>Posted by the Rohit Bhandari</p>
+            <p className='grey-text'>26rd august , 2am</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProjectList;
