0ed6c6770950edf00018a486e5cc529f63d91305
unknown <rohit.raj.bhandari07@gmail.com>
2020-08-28 00:51:17 +0300

    added method: Compose() function

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { getFirebase } from "redux-firestore";
-import { getFirestore } from "react-redux-firebase";
+import { reduxFirestore, getFirebase } from "redux-firestore";
+import { reactReduxFirebase, getFirestore } from "react-redux-firebase";
+import firebaseConfig from "./config/firebaseConfig";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
