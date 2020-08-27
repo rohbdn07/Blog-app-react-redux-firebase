16c190e713cde77450a9865556189dc7b147c14d
unknown <rohit.raj.bhandari07@gmail.com>
2020-08-27 23:52:50 +0300

    dispatch and reducer

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -14,7 +14,8 @@ class CreateProject extends Component {
   };
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    // console.log(this.state);
+    this.props.createProject(this.state);
   };
   render() {
     return (
