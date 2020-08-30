c3344a821d63e93783f29e9259fce016ce63866f
unknown <rohit.raj.bhandari07@gmail.com>
2020-08-30 15:31:11 +0300

    signIn to firebase sucessfull

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,6 +1,14 @@
 export const signIn = (credentials) => {
   return (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
-    firebase.auth().signInWithEmailAndPassword();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({ type: "LOGIN_SUCCESS" });
+      })
+      .catch((err) => {
+        dispatch({ type: "LOGIN_ERROR" }, err);
+      });
   };
 };
